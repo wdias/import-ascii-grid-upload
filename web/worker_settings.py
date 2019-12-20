2ff3472ca83be66c88a3e911a782db5a8e859c10
gihankarunarathne <gckarunarathne@gmail.com>
2019-12-20 15:37:45 +0530

    Fixed: OPTIMIZE_STORAGE

--- web/worker_settings.py
+++ web/worker_settings.py
@@ -6,7 +6,7 @@ REDIS_HOST = 'adapter-redis-master.default.svc.cluster.local'
 REDIS_PORT = 6379
 REDIS_DB = 3
 REDIS_PASSWORD = 'wdias123'
-OPTIMIZE_STORAGE: bool = os.getenv('OPTIMIZE_STORAGE', True)
+OPTIMIZE_STORAGE: bool = os.getenv('OPTIMIZE_STORAGE', '1') == '1'
 
 # Queues to listen on
 QUEUES = [os.getenv('HOSTNAME', 'default')]
